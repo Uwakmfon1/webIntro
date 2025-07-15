6df9c5126b9b83ac707abdb6c9494d7e7a048cb2
Uwakmfon1 <udohuwakmfon2@gmail.com>
2025-07-15 14:10:57 +0100

    forms tuts

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+// var_dump(isset($_POST));
+echo $_POST['name'];
+// if (isset($_POST["submit-btn"])) 
+// { $post_data = file_get_contents('php://input');
+//      echo "<div> POST BODY <br>";
+//      var_dump($post_data);
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+</body>
+</html>
