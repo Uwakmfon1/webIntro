01a33a3453f3931ff73d288b1fa5dc8818bf1132
Uwakmfon1 <udohuwakmfon2@gmail.com>
2025-08-16 06:00:07 +0100

    added fruits loop-display

--- index.js
+++ index.js
@@ -1,63 +1,25 @@
-console.log('Hello world');
 
-// +, -, *, /, %, ==, ===
-// variables definition
-// data types 
-// keywords
-
-// String, number, boolean, null, undefined, BigInt, Symbol, Object 
-
-
-
-let data = {
-    "name": "Raymond",
-    "occupation": "programmer",
-    "gender": "Male",
-    "race": "caucasian",
-    "nationality": "Nigerian"
-}
 
 console.log(data);
 
 let fruits = [
     "apple", "water melon", "pineapple", "mango", "orange", "cucumber", "banana", "strawberry", "Grape", "paw paw", "Avocado", "pear",
-
-    { "name": "Fig", "botanical_name": "botanyy spp", "region_found": "South America" }
-
 ];
 
-console.log(fruits[12]['region_found']);
+const ol = document.getElementById("parentList");
 
-console.log(fruits);
-const fruitList = document.getElementById("childList")
+fruits.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    ol.appendChild(li);
+}); 
 
-for (i = 0; i <= fruits.length; i++) {
-    fruitList.innerHTML = fruits;
-    i++;
+let num1 = 32;
+
+while(num1 >=32 && num1 <=40)
+{
+    console.log(num1);
+    num1++;
 }
 
-// camelCase
-// snake_case
-
-// CamelCase
-
-// const MyName = "Etoro";
-
-let YourName = "Lilian";
-
-var MyName = "Goody";
-
-// const name = "David";
-
-// MyName= "Gideon";
-
-let clientName = '';
-
-console.log("Hi " + MyName + ". We are so glad to have you. You'll soon hear from us. Best regards: " + YourName)
-
-let age = null;
-
-age = 23;
-
-
-console.log(age); 
+console.log("active")
